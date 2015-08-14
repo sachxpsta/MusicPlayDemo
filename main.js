97a94a87cc2939f51b31674519c3801cd6fd6f3b
unknown <yulecheng@gmail.com>
2015-08-14 22:31:10 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lecheng on 2015/8/14.
+ */
+var app = angular.module('myApp',[]);
+
+app.controller('PlayerController',['$scope',function($scope){
+
+}]);
+
+app.controller('RelatedController',['$scope', function ($scope) {
+
+}]);
